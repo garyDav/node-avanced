dfe985c0c4970427f045677b9dfdb46b20e8b9ed
garyDav <gary.2810.dav@gmail.com>
2022-10-06 15:22:12 -0400

    add codigoArduino

--- Arduino.ino
+++ Arduino.ino
@@ -8,15 +8,14 @@
 
 // Update these with values suitable for your network.
 byte mac[] = {0xDE, 0xED, 0xBA, 0xFE, 0xFE, 0xED};
-IPAddress server(192, 168, 0, 108); // Ip broker
+IPAddress server(192, 168, 0, 108); // ip broker
 
 const byte pinDht = 2;
 const byte pinStierra = A0;
 const byte pinLdr = A1;
 const byte pinLluvia = 3;
 const byte motor = 5;
-const byte led = 12;
-
+const byte led = 7;
 int valorLluvia;
 int valorTierra;
 int valorLdr;
@@ -38,21 +37,21 @@ PubSubClient client(server, 1883, callback, ethClient);
 // Callback function
 void callback(char *topic, byte *payload, unsigned int length)
 {
+
   // turn the LED ON if the payload is '1' and publish to the MQTT server a confirmation message
-  if (payload[0] == 100)
+  if (payload[0] == 101)
   {
     digitalWrite(motor, LOW);
     digitalWrite(led, HIGH);
     client.publish(broker, "Motor On");
   }
-  Serial.println("Motor - Led -> ON ");
+
   // turn the LED OFF if the payload is '0' and publish to the MQTT server a confirmation message
-  if (payload[0] == 101)
+  if (payload[0] == 100)
   {
     digitalWrite(motor, HIGH);
     digitalWrite(led, LOW);
     client.publish(broker, "Motor Off");
-    Serial.println("Motor - Led -> OFF ");
   }
 } // void callback
 
@@ -62,19 +61,19 @@ void publishMessage(const char *topic, String payload, boolean retained)
     Serial.println("Message publised [" + String(topic) + "]: " + payload);
 }
 
-String formatAgent(String uuid, String type, String value, String name2 = "Sensor", String username = "Arduino", int pid = 100, String hostname2 = "localhost")
+String formatAgent(String uuid, String type, String value, String name = "Sensor", String username = "Arduino", int pid = 100, String hostname = "localhost")
 {
   String result = "";
   result.concat("{\"agent\": {\"uuid\":\"");
   result.concat(uuid);
   result.concat("\", \"name\": \"");
-  result.concat(name2);
+  result.concat(name);
   result.concat("\", \"username\": \"");
   result.concat(username);
   result.concat("\", \"pid\": ");
   result.concat(pid);
   result.concat(", \"hostname\": \"");
-  result.concat(hostname2);
+  result.concat(hostname);
   result.concat("\"}, \"metrics\": [{\"type\": \"");
   result.concat(type);
   result.concat("\", \"value\": \"");
@@ -125,48 +124,34 @@ void loop()
   valorLluvia = digitalRead(pinLluvia);
   if (valorLluvia == HIGH)
   {
-    valorLluvia = 0;
+    valorLluvia = 1;
   }
   else
   {
-    valorLluvia = 1;
+    valorLluvia = 0;
   }
   // Mostramos los datos en pantalla
   lcd.setCursor(0, 0);
-  lcd.print("T:");
+  lcd.print("T");
   lcd.print(temperatura);
-  lcd.print("C ");
-  lcd.print("H:");
+  lcd.print("C");
+  lcd.print(" ");
+  lcd.print("H");
   lcd.print(humedad);
-  lcd.print("% ");
-  lcd.print("L:");
+  lcd.print(" ");
+  lcd.print("Luz");
   lcd.print(valorLdr);
-  lcd.print("% ");
+  lcd.print(" ");
   lcd.setCursor(0, 1);
-  lcd.print("H.Sue:");
+  lcd.print("HS");
   lcd.print(valorTierra);
-  lcd.print("% ");
-  lcd.print("Lluv:");
+  lcd.print(" ");
+  lcd.print("Lluvia");
   lcd.print(valorLluvia);
-  Serial.print("Temperatura: ");
-  Serial.print(temperatura);
-  Serial.println("°C");
-  Serial.print("Humedad: ");
-  Serial.print(humedad);
-  Serial.println("%");
-  Serial.print("Luz: ");
-  Serial.print(valorLdr);
-  Serial.println("%");
-  Serial.print("Humedad Suelo: ");
-  Serial.print(valorTierra);
-  Serial.println("%");
-  Serial.print("Lluvia: ");
-  Serial.println(valorLluvia);
-  Serial.println("");
-  publishMessage(broker, formatAgent("Temp", " C", String(temperatura), "Temperatura"), true);
-  publishMessage(broker, formatAgent("Hum", " por ciento", String(humedad), "Humedad"), true);
-  publishMessage(broker, formatAgent("Luz", " por ciento", String(valorLdr), "Luz solar"), true);
-  publishMessage(broker, formatAgent("HSuelo", " por ciento", String(valorTierra), "Humedad del suelo"), true);
-  publishMessage(broker, formatAgent("Ll", " 1 = SI && 0 = NO", String(valorLluvia), "Lluvia"), true);
-  delay(2000);
+  publishMessage(broker, formatAgent("T", " C", String(temperatura), "Temperatura"), true);
+  publishMessage(broker, formatAgent("H", " por ciento", String(humedad), "Humedad"), true);
+  publishMessage(broker, formatAgent("LDR", " por ciento", String(valorLdr), "Luz solar"), true);
+  publishMessage(broker, formatAgent("HT", " por ciento", String(valorTierra), "Humedad de la tierra"), true);
+  publishMessage(broker, formatAgent("LL", " 1 = SI && 0 = NO", String(valorLluvia), "Lluvia"), true);
+  delay(3000);
 }
